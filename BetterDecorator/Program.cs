5dbabe6794ee060fc622a3842aad94b4ecf774fb
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-13 15:34:53 +0000

    Finished Decor metod

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = args[0];
+            char 
+            Console.WriteLine(Decor
+            
+
+        }
+
+        private static string Decor(string s, char dec, int n_vezes)
+        {
+            string decoracao = new string(dec, n_vezes);
+            return $"{decoracao} {s} {decoracao}";
+
+
+        }
+
+    }
+}
