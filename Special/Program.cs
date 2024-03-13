06c9edd3798c1c079755d90a7926403fba15c6e9
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-13 16:27:59 +0000

    Making the code layout

---
+++ Special/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int user = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(user));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            int result;
+            if (n == 1)
+            {
+                result = 1;
+            }
+            else
+                result = 1-n + 2 * n;
+            return result;
+        }
+    }
+}
