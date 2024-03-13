d52fa59c5eecdb1ed9246f722bc3fa462331c4d3
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-13 15:48:51 +0000

    Exercise 2 Completed

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -4,6 +4,10 @@ namespace BetterDecorator
 {
     class Program
     {
+        /// <summary>
+        /// Buscar os 3 argumentos e converter em tipos de valor e referencia!
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string frase = args[0];
@@ -12,10 +16,22 @@ namespace BetterDecorator
             
             Console.WriteLine(Decor(frase, type_of_char, rep));
         }
-
+        
+        /// <summary>
+        /// Fazer com que os caracteres se repitam.
+        /// </summary>
+        /// <param name="s"></param> Frase
+        /// <param name="dec"></param> Caracteres
+        /// <param name="n_vezes"></param> Numero de vezes que os caracteres
+        /// vão se repetir!
+        /// <returns></returns> Os caracteres da string decoracao repetem-se
+        /// tendo em conta o numero de vezes que foi atribuido. E por fim
+        /// no meio fica a frase atribuida
         private static string Decor(string s, char dec, int n_vezes)
         {
-            string decoracao = new string(dec, n_vezes);
+            string decoracao = new string(dec, n_vezes); 
+            //dec = carater | n_vezes = que vai ser impresso esse caracter
+
             return $"{decoracao} {s} {decoracao}";
 
         }
